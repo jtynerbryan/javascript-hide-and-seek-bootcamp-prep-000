114322328d75c8fa2657b412d78a1f29a95dd6d1
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-01-30 19:16:55 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested div.target ')
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0, l = lis.length; i < l; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+  return lis
+}
+
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div')
+}
